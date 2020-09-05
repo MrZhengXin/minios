6aea49b20c6e2fe081c9eb7b6023e16b4ff514cc
Guaishou74851 <chenbin74851@126.com>
2020-09-05 11:14:28 +0800

    add support to Linux

--- shell.py
+++ shell.py
@@ -3,15 +3,19 @@
 import datetime
 import os
 import re
+import platform
 from time import sleep
 
 
 class Shell:
     def __init__(self):
-        self.print_system_info()
         self.block_flag = False
-
+        platform_to_clear_cmd = {'Windows': 'cls', 'Linux': 'clear'}
+        self.clear_cmd_str = platform_to_clear_cmd[platform.system()]
+        self.print_system_info()
+               
     def print_system_info(self):
+        os.system(self.clear_cmd_str)
         print('MiniOS 1.0', datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S'))
 
     # current working directory
@@ -24,14 +28,20 @@ class Shell:
             raw_command = commands[i].split()
             if len(raw_command) == 0:
                 continue
+            re_flag = False
+            cur_cmd = raw_command[0]
+            if cur_cmd == 're':
+                re_flag = True
+                raw_command = raw_command[1:]
             commands[i] = [raw_command[0]]
             for arg in raw_command[1:]:
                 match_flag = False
-                for file_name in file_list:
-                    match_res = re.match(arg + '$', file_name)
-                    if match_res:
-                        match_flag = True
-                        commands[i].append(match_res.group(0))
+                if re_flag:
+                    for file_name in file_list:
+                        match_res = re.match(arg + '$', file_name)
+                        if match_res:
+                            match_flag = True
+                            commands[i].append(match_res.group(0))
                 if match_flag is False:
                     commands[i].append(arg)
         # print(commands)
@@ -43,6 +53,6 @@ class Shell:
     def block(self, func, interval=1):
         self.block_flag = True
         while self.block_flag:
-            os.system("cls")
+            os.system(self.clear_cmd_str)
             func()
             sleep(interval)
